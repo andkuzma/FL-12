dcf1945366653a8c7ecf9a069359f184849b25f6
Andrii Kuzma <51418233+andkuzma@users.noreply.github.com>
2019-12-24 20:25:33 +0200

    Update task1

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,24 +1,29 @@
-let expr = prompt('Quadratic equation', ' ');
-let str = expr.split(',');
-let a = Number(str[0]);
-let b = Number(str[1]);
-let c = Number(str[1+1]);
-let d = b*b - (a+a+a+a) * c;
-let x1 = 0, x2 = 0;
+let a = prompt('Please enter a');
+let b = prompt('Please enter b');
+let c = prompt('Please enter c');
+
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+let z = 4;
+let k = 2;
+let d = b*b - z * a * c;
+let x1;
+let x2;
 console.log('a= ' + a);
 console.log('b= ' + b);
 console.log('c= ' + c);
 console.log('d= ' + d);
-if (d > 0) {
-    x1 = (-b + Math.sqrt(d)) / (a + a);
-    x2 = (-b - Math.sqrt(d)) / (a + a);
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log('Invalid input data');
+} else if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (k * a);
+    x2 = (-b - Math.sqrt(d)) / (k * a);
     console.log('value x1 ==' + x1);
     console.log('value x2 ==' + x2);
-} else if (d < 0 || isNaN(a) || isNaN(b) || isNaN(c)) {
-    console.log('Invalid input data');
 } else if (d === 0) {
-    x1 = -b / (a + a);
-    console.log('d=0 ___ x1=' + x1);
-} else if (isFinite(d) || d !== null || str.length>0) {
+    x1 = -b / (k * a);
+    console.log('Discriminant=0 ___ x1=' + x1);
+} else {
     console.log('No solution');
 }
