b7cc508c103fc84bc7e2bccaa0f14e5eeded9fed
Andrii Kuzma <51418233+andkuzma@users.noreply.github.com>
2019-12-24 20:40:13 +0200

    Update task2

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,14 +1,17 @@
-let expr = prompt('Identify triangle type', ' ');
-let str = expr.split(',');
-let a = Number(str[0]);
-let b = Number(str[1]);
-let c = Number(str[1 + 1]);
+let a = prompt('Please enter a');
+let b = prompt('Please enter b');
+let c = prompt('Please enter c');
+
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('input values should be ONLY numbers ');
-} else if (a === 0 || b === 0 || c === 0) {
+} else if ( a < 0 || b < 0 || c < 0 || a === 0 || b === 0 || c === 0) {
     alert('A triangle must have 3 sides with a positive definite length');
 }else if (a + b < c || b + c < a || a + c < b) {
-    console.log('Triangle doesn’t exist');
+    console.log('Triangle doesnï¿½t exist');
 }else if (a === b && b === c) {
     console.log('Equilateral triangle');
 } else if (a === b && a !== c || b === c && b !== a || a === c && b !== c) {
